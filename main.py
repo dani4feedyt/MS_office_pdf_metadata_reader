9fab7745e6f096230790c90f5554c0806b16d5ea
dani4feed <dan.arbatov@ukr.net>
2025-05-22 00:41:41 +0300

    0.3 stable

--- main.py
+++ main.py
@@ -5,8 +5,7 @@ from docx import Document
 from tkinter import filedialog
 import tkinter as tk
 
-b_down = False
-jobid = None
+file_path = None
 
 def get_excel_metadata(file_path):
     workbook = load_workbook(file_path, keep_vba=False)
@@ -53,42 +52,55 @@ def get_file_metadata(file_path):
         metadata['LastModifiedTime'] = last_modified
 
         return metadata
+
     except Exception as e:
         return f"Error: {str(e)}"
 
 
 def change_pic_down(event):
-    global b_down
-    if not b_down:
-        button = event.widget
+    button = event.widget
+    if str(button.winfo_name()) == "!button":
         activated_img = tk.PhotoImage(file='assets/button_choose_A.png')
         button.configure(image=activated_img)
         button.image = activated_img
-
-        b_down = True
-        print("downnn")
         open_file()
+
+    elif str(button.winfo_name()) == "!button2":
+        activated_img = tk.PhotoImage(file='assets/button_extract_A.png')
+        button.configure(image=activated_img)
+        button.image = activated_img
+        extract_metadata(file_path)
 
 
 def change_pic_up(event):
-    global b_down
-    if b_down:
-
-        print("upnn")
+    button = event.widget
+    if str(button.winfo_name()) == "!button":
         button = event.widget
         deactivated_img = tk.PhotoImage(file='assets/button_choose.png')
         button.configure(image=deactivated_img)
         button.image = deactivated_img
-        b_down = False
+    elif str(button.winfo_name()) == "!button2":
+        activated_img = tk.PhotoImage(file='assets/button_extract.png')
+        button.configure(image=activated_img)
+        button.image = activated_img
 
 
 def open_file():
 
-    file_path = filedialog.askopenfilename(filetypes=[
+    local_file_path = filedialog.askopenfilename(filetypes=[
         ("Supported Files", "*.xlsx *.docx *.doc"),
         ("Excel Files", "*.xlsx"),
         ("Word Files", "*.docx *.doc")
     ])
+    global file_path
+    file_path = local_file_path
+    if file_path:
+        text_box.insert(tk.END, f'File <<{file_path}>> loaded successfully!')
+    else:
+        text_box.insert(tk.END, 'Error, please select a file!')
+
+
+def extract_metadata(file_path):
     if file_path:
         metadata = get_file_metadata(file_path)
         if isinstance(metadata, dict):
@@ -117,10 +129,9 @@ true_bg = "#222831"
 root.config(bg=true_bg)
 
 b_img = tk.PhotoImage(file='assets/button_choose.png')
-b_img_a = tk.PhotoImage(file='assets/button_choose_A.png')
 open_button = tk.Button(root, image=b_img, bg=true_bg, borderwidth=0, relief="solid", activebackground=true_bg)
 open_button.image = b_img
-#open_button.configure(command=lambda: change_pic_down(open_button))
+
 open_button.bind("<ButtonPress-1>", change_pic_down)
 open_button.bind("<ButtonRelease-1>", change_pic_up)
 
@@ -131,8 +142,14 @@ text_box = tk.Text(root, height=20, width=70, bg="#393E46", fg="white",
                    font=("Consolas", 10), wrap=tk.WORD)
 text_box.pack(pady=10)
 
-exit_button = tk.Button(root, text="Exit", command=root.quit,
-                        bg="#DFD0B8", fg="#222831", font=("Arial", 12, "bold"), activebackground="#948979")
-exit_button.pack(pady=10)
+b2_img = tk.PhotoImage(file='assets/button_extract.png')
+
+b_extract = tk.Button(root, image=b2_img, bg=true_bg, borderwidth=0, relief="solid", activebackground=true_bg)
+
+b_extract.bind("<ButtonPress-1>", change_pic_down)
+b_extract.bind("<ButtonRelease-1>", change_pic_up)
+
+b_extract.image = b2_img
+b_extract.pack(pady=10)
 
 root.mainloop()
